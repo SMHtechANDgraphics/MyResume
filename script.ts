a1f9ad1835d60fed37366cf918e92864cdf989f8
SMHtechANDgraphics <smhtechbuisness@gmail.com>
2024-09-15 00:02:22 +0500

    My-CV
    SMHtechANDgraphics

---
+++ script.ts
@@ -0,0 +1,19 @@
+// 'const contactInfo as htmlbutton= document.getElementById("contactInfo skills");
+// const skills = document.getElementById("Professional Skills");
+
+// if(professional Skills)'
+
+const toggleButto = document.getElementById(
+    "toggle-skills"
+  ) as HTMLButtonElement;
+  
+//   const skills = document.getElementById("skills") as HTMLElement;
+  
+//   toggleButton.addEventListener("click", () => {
+//     if (skills.style.display === "none") {
+//       skills.style.display = "block";
+//     } else {
+//       skills.style.display = "none";
+//     }
+//   });
+  
